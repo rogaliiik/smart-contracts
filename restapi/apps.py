bed16daf9c78634dee8736cf51e53c1ab62904e4
Artyom Galkin <igalart2000@gmail.com>
2023-02-15 12:22:55 +0300

    Rename main directories

--- restapi/apps.py
+++ restapi/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NftDrfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nft_drf'
+    name = 'restapi'
--- restapi/migrations/0003_alter_token_media_url_alter_token_owner_and_more.py
+++ restapi/migrations/0003_alter_token_media_url_alter_token_owner_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('nft_drf', '0002_alter_token_owner'),
+        ('restapi', '0002_alter_token_owner'),
     ]
 
     operations = [
